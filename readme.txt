7f267231f26beb6d6ee8b67bb5fecbbf1daff648
figurebelow <figurebelow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-17 20:00:53 +0000

    First version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-d3/trunk@669387 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Wp-D3 ===
+Contributors: Ruben Afonso<ruben@figurebelow.com>
+Donate link: http://www.figurebelow.com
+Tags: d3, visualization, javascript
+Requires at least: 3.0
+Tested up to: 3.4
+Stable tag: 1.0.0
+
+A plugin to integrate D3 into your Wordpress post/pages.
+== Description ==
+D3.js is a JavaScript library for manipulating documents based on data. D3 helps you bring data to life using HTML, SVG and CSS. D3’s emphasis on web standards gives you the full capabilities of modern browsers without tying yourself to a proprietary framework, combining powerful visualization components and a data-driven approach to DOM manipulation. 
+
+Ths plugin provides the current last version of D3 (v3.3) and a couple of tags to reference any possible .js or .css depedencies and to paste javascript code into your post or pages to render it.
+All extra .css or .js files can be uploaded to your Wordpress blog and then included into the post/page easily using built-in functions.
+Enjoy!.
+
+== Installation ==
+1. Upload `wp-d3/` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the tags [d3-link][/d3-link].
+3. If your code depends on .js, .css, .json third files:
+    Click between the tags to place the mouse cursor.
+    Add the deps files through 'Add Media' button, select them, and then click on 'Insert into page'.
+    The returned links should be now between the tags [d3-link] and [/d3-link] tags. 
+    Example: [d3-link]
+             <a href="http://figurebelow.com/wp-content/uploads/2013/02/mixes.js">mixes</a>
+             [/d3-link]
+4. Paste your d3 code between [d3-source] and [/d3-source] tags.
+   Yoy can specify the canvas name, where the code is placed, adding the 'canvas' parameter e.g. [d3-source canvas="xxx"], 
+   where xxx is the canvas name. The default id is "canvas".
+5. Save the post/page.
+6. Enjoy!
+
+== Frequently Asked Questions == 
+Lets wait until next version.
+
+== Upgrade Notice == 
+* Update Wordpress-d3 to version 1.0, first implementation.
+
+== Screenshots ==
+Not necessary.
+
+== Changelog ==
+= 1.0.0 =
+* First commit. Basic tag functionality, [d3-link], [d3 canvas="canvas"]
+
---
+++ wp-d3.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: Wordpress-d3.js
+Plugin URI: http://www.figurebelow.com/wp-d3
+Description: D3 is a very popular visualization library written in Javascript. This plugins provides a set of tags to link page/post content to d3.js libraries in order to visualize the javascript snippets inside Wordpress.
+All javascript code can be update as "Media" to the Wordpress blog and then refere
+Version: 1.0.0
+Author: Ruben Afonso
+Author URI: http://www.figurebelow.com
+License: GPL2
+*/
+
+/* Init
+ *
+ */
+function wordpressd3_init() {
+	wp_deregister_script('wordpress-d3');
+	wp_enqueue_script('wordpress-d3', true, '1.0.0');
+}
+
+/**
+ * Include .js and .css resources from [d3-links] tag
+ */
+function include_resources ($attr, $content) {
+	// add the d3 link
+	echo '<script type="text/javascript" src=\'' . plugins_url('/js/d3.v3.js', __FILE__) . '\'></script>';
+	$hrefs = array();
+	$dom = new DOMDocument();
+	$dom->loadHTML($content);
+	$tags = $dom->getElementsByTagName('a');
+	foreach ($tags as $tag) {
+       		$hrefs[] =  $tag->getAttribute('href');
+	}
+	foreach ($hrefs as $js)
+	{
+	  if (substr_compare($js, "js", -strlen("js"), strlen("js")) === 0)
+	  {
+	     echo '<script type="text/javascript" ';
+             echo 'src=\'' . $js . '\'></script>';
+	  }
+          if (substr_compare($js, "css", -strlen("css"), strlen("css")) === 0)
+	  {
+	     echo '<link type="text/css" rel="Stylesheet" ';
+             echo 'href=\'' . $js . '\'/>';
+	  }
+	}
+}
+
+/**
+ * Creates a div around the javascript code to visualize it.
+ * Accepts the canvas="xxx" parameter to specify the canvas name. By default it is
+ * created with id='canvas' (so creative uh ;-))
+ */
+function print_source ($attr, $content) {
+	extract(shortcode_atts(array(
+	      'canvas' => 'canvas'), $attr));
+	$chart = $canvas;
+	echo '<div class="' . $chart . '">';
+	echo '<script type="text/javascript">';
+	echo $content . '</script>' . '</div>';
+}
+
+add_action('init', 'wordpressd3_init');
+add_shortcode("d3-link", "include_resources");
+add_shortcode("d3-source", "print_source");
+
+// Remove WordPress auto-p and wptexturize from being executed before wordpress-d3
+remove_filter('the_content', 'wpautop' );
+remove_filter('the_content', 'wptexturize');
+// and added again with less priority
+add_filter( 'the_content', 'wpautop' , 99);
+add_filter( 'the_content', 'wptexturize' , 99);
+?>
+
