bc839e2aab3c665f2d75b72ec842ea774f220c7f
figurebelow <figurebelow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-07 10:38:06 +0000

    Added 1.2 version
    git-svn-id: https://plugins.svn.wordpress.org/wp-d3/tags/1.2@768181 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-d3.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: Wordpress-d3.js
+Plugin URI: http://wordpress.org/extend/plugins/wp-d3/
+Description: D3 is a very popular visualization library written in Javascript. This plugins provides a set of tags to link page/post content to d3.js libraries in order to visualize the javascript snippets inside Wordpress.
+All javascript code can be update as "Media" to the Wordpress blog.
+Version: 1.2
+Author: Ruben Afonso
+Author URI: http://www.figurebelow.com
+License: GPL2
+*/
+
+/* 
+ * Init
+ */
+function wordpressd3_init() {
+	wp_deregister_script('wordpress-d3');
+	wp_enqueue_script('wordpress-d3', true, '1.0.0');
+}
+
+/**
+ * Include .js and .css resources from [d3-links] tag
+ */
+function include_resources ($attr, $content) {
+	// add the d3 link
+	$result = '<script type="text/javascript" src=\'' . plugins_url('/js/d3.v3.min.js', __FILE__) . '\'></script>';
+	$hrefs = array();
+	$dom = new DOMDocument();
+	if (!empty($content)) {
+		$dom->loadHTML($content);
+		$tags = $dom->getElementsByTagName('a');
+		foreach ($tags as $tag) {
+       			$hrefs[] =  $tag->getAttribute('href');
+		}
+		foreach ($hrefs as $js) {
+	  		if (substr_compare($js, "js", -strlen("js"), strlen("js")) === 0) {
+	     		$result = $result . '<script type="text/javascript" ';
+	            $result = $result . 'src=\'' . $js . '\'></script>';
+			}
+          	if (substr_compare($js, "css", -strlen("css"), strlen("css")) === 0) {
+	     		$result = $result . '<link type="text/css" rel="Stylesheet" ';
+             	$result = $result . 'href=\'' . $js . '\'/>';
+	  		}
+		}
+	}
+    return $result;
+}
+
+/**
+ * Creates a div around the javascript code to visualize it.
+ * Accepts the canvas="xxx" parameter to specify the canvas name. By default it is
+ * created with id='canvas' (so creative uh ;-))
+ */
+function print_source ($attr, $content) {
+	extract(shortcode_atts(array('canvas' => 'canvas'), $attr));
+	$chart = $canvas;
+	$code = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n\']+/", "\n", $content); // remove empty lines
+	$result = '<div class="' . $chart . '">' . '<script type="text/javascript">' . $code . '</script>' . '</div>';
+	return $result;
+}
+
+/**
+* This function avoids wptexturize from modifying the d3 code,
+* just in case remove_filter() doesn't work as expected.
+*/ 
+function skip_d3_source($shortcodes){
+    $shortcodes[] = 'd3-source';
+    return $shortcodes;
+}
+add_filter( 'no_texturize_shortcodes', 'skip_d3_source' );
+
+// remove wpautop and wptexturize for a while
+remove_filter('the_content', 'wptexturize');
+remove_filter('the_content', 'wpautop' );
+
+add_action('init', 'wordpressd3_init');
+add_shortcode("d3-link", "include_resources");
+add_shortcode("d3-source", "print_source");
+
+// and added again with less priority, this doesn't seems
+add_filter( 'the_content', 'wpautop' , 99);
+add_filter( 'the_content', 'wptexturize' , 99);
+?>
